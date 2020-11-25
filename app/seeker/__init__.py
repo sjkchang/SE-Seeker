7b1c8886cc3b0a8360f090610cb11d8258afc35f
sjkchang <sjkchang@gmail.com>
2020-11-24 18:02:57 -0800

    Added user functionality, started on the UI

--- app/seeker/__init__.py
+++ app/seeker/__init__.py
@@ -6,8 +6,12 @@ The core module of my project
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
 
 
 def create_app(testing):
@@ -27,12 +31,13 @@ def create_app(testing):
         app.config.from_object('config.TestConfig')
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     with app.app_context():
         # Include our routes
-        from .routes import home, internship
+        from .routes import home, internship, userManagement
         from .models.Internship import Internship
-        db.drop_all()
-        db.create_all()
+        #db.drop_all()
+        #db.create_all()
 
         return app
